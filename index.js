1ad7ba9abf5d0606f206a02e9b3670fe5aa9e987
Josh Morris <jmo0624@gmail.com>
2020-07-09 23:10:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // Write your solution here!
-const append = []
+const append = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
+const removeLast = []
 append.push('Odie')
